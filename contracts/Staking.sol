e4442842c077a66363e73d8b2d6b26c9fd329713
Dustin Newland <dustin@ardentcreative.com>
2021-09-24 12:47:24 -0500

    Initial commit

---
+++ contracts/Staking.sol
@@ -0,0 +1,169 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import "../libs/Utility.sol";
+import "hardhat/console.sol";
+
+contract Ownable {
+    address public owner;
+    event OwnershipRenounced(address indexed previousOwner);
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipRenounced(owner);
+        owner = address(0);
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+        _transferOwnership(_newOwner);
+    }
+
+    function _transferOwnership(address _newOwner) internal {
+        require(_newOwner != address(0));
+        emit OwnershipTransferred(owner, _newOwner);
+        owner = _newOwner;
+    }
+}
+
+contract Staking is Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
+    IERC20 public stakingToken;
+    uint256 private _totalSupply;
+    uint256 private _periodFinish;
+    mapping(address => uint256) private _stake;
+    mapping(address => uint256) private _updated;
+
+    event Recovered(address token, uint256 amount);
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event StakingEnds(uint256 newTimestamp);
+
+    constructor(address _stakingToken) {
+        stakingToken = IERC20(_stakingToken);
+        _periodFinish = block.timestamp + 31536000;
+        emit StakingEnds(_periodFinish);
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address _user) external view returns (uint256) {
+        return _stake[_user];
+    }
+
+    function _lastTimeRewardApplicable() internal view returns (uint256) {
+        return
+            block.timestamp < _periodFinish ? block.timestamp : _periodFinish;
+    }
+
+    function total() external view hasBalance(msg.sender) returns (uint256) {
+        return _stake[msg.sender] + _getNewRewards(msg.sender);
+    }
+
+    function stake(uint256 amount)
+        external
+        nonReentrant
+        inStakingPeriod
+        updateRewards(msg.sender)
+    {
+        _totalSupply += amount;
+        _stake[msg.sender] += amount;
+        stakingToken.transferFrom(msg.sender, address(this), amount);
+        emit Staked(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount)
+        public
+        nonReentrant
+        hasBalance(msg.sender)
+        updateRewards(msg.sender)
+    {
+        require(amount > 0, "You must specify a positive amount of money");
+        require(_stake[msg.sender] >= amount, "Not enough tokens");
+        _withdraw(msg.sender, amount);
+    }
+
+    function withdrawAll()
+        public
+        nonReentrant
+        hasBalance(msg.sender)
+        updateRewards(msg.sender)
+    {
+        _withdraw(msg.sender, _stake[msg.sender]);
+    }
+
+    function _withdraw(address to, uint256 amount)
+        private
+        hasBalance(to)
+        updateRewards(msg.sender)
+    {
+        require(amount <= _totalSupply, "Not enough tokens");
+        _totalSupply -= amount;
+        _stake[to] -= amount;
+        stakingToken.transfer(to, amount);
+        emit Withdrawn(to, amount);
+    }
+
+    function rewardsOf(address addr) public view returns (uint256) {
+        return _getNewRewards(addr);
+    }
+
+    function _getNewRewards(address addr) private view returns (uint256) {
+        uint256 etime = _lastTimeRewardApplicable() - _updated[addr];
+        if (etime == 0 || _stake[addr] == 0) {
+            return 0;
+        }
+        // _ratio = 6341958397; // 20% apr : This represents === Rate / Time * 10^18 === .20 / 31536000 * 10^18
+        uint256 reward = Utility.compound(_stake[addr], 6341958397, etime);
+        return reward - _stake[addr];
+    }
+
+    function setFinish(uint256 _finish) external onlyOwner {
+        _periodFinish = _finish;
+        emit StakingEnds(_finish);
+    }
+
+    function recoverERC20(address addr, uint256 amt) public onlyOwner {
+        IERC20 theToken = IERC20(addr);
+        theToken.transfer(msg.sender, amt);
+        emit Recovered(addr, amt);
+    }
+
+    modifier updateRewards(address addr) {
+        uint256 newRewards = _getNewRewards(addr);
+        _updated[addr] = _lastTimeRewardApplicable();
+        if (newRewards > 0) {
+            _stake[addr] += newRewards;
+            _totalSupply += newRewards;
+            emit Staked(addr, newRewards);
+        }
+        _;
+    }
+
+    modifier inStakingPeriod() {
+        require(block.timestamp <= _periodFinish, "Staking period has ended");
+        _;
+    }
+
+    modifier hasBalance(address addr) {
+        require(_stake[addr] > 0, "No balance");
+        _;
+    }
+}
---
+++ test/staking.js
@@ -0,0 +1,191 @@
+const { expect } = require("chai");
+const qty = "100000000000000000000000";
+const month_in_seconds = 2628000;
+const r = {
+  m1: "1680633033310046341168",
+  m2: "3389511340546621956208",
+  m12m1: "22140274964779807753125",
+  m12: "22140275739388350171449",
+};
+describe("Staking Contract", function () {
+  let CarrContract;
+  let StakingContract;
+  let CarrToken;
+  let Staking;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
+  let depositTime;
+  let walletOwner;
+  let wallet1;
+  let wallets;
+
+  before(async function () {
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    CarrContract = await ethers.getContractFactory("CARR");
+    // [walletOwner, wallet1, ...wallets] = ethers.getWallets();
+    StakingContract = await ethers.getContractFactory("Staking");
+    CarrToken = await CarrContract.deploy();
+    Staking = await StakingContract.deploy(CarrToken.address);
+    await CarrToken.transfer(addr1.address, qty);
+  });
+  describe("debug", async function() {
+    it("Does not accept ether", async function() {
+      const params = {to: Staking.address, value: ethers.utils.parseEther("1.0")};
+      await expect(owner.sendTransaction(params)).to.be.reverted;
+      await expect(addr1.sendTransaction(params)).to.be.reverted;
+    });
+  });
+  describe("Deployment", async function () {
+    describe("Ownership", async function () {
+      it("Is owned by the deployer", async function () {
+        expect(await Staking.owner()).to.equal(owner.address);
+      });
+      it("Allows successful ownership transfer", async function () {
+        await expect(Staking.transferOwnership(addr1.address)).to.emit(Staking, "OwnershipTransferred").withArgs(owner.address, addr1.address);
+        expect(await Staking.owner()).to.equal(addr1.address);
+      });
+      it("Allows ownership to revert back", async function () {
+        await expect(Staking.connect(addr1).transferOwnership(owner.address)).to.emit(Staking, "OwnershipTransferred").withArgs(addr1.address, owner.address);
+        expect(await Staking.owner()).to.equal(owner.address);
+      });
+    });
+    describe("Balances", async function () {
+      it("Has no balance for the owner", async function () {
+        expect(await Staking.balanceOf(owner.address)).to.equal(0);
+      });
+      it("Has no rewards for the owner", async function () {
+        expect(await Staking.rewardsOf(owner.address)).to.equal(0);
+      });
+      it("Has no balance for regular users", async function () {
+        expect(await Staking.balanceOf(addr1.address)).to.equal(0);
+      });
+      it("Has no rewards for regular users", async function () {
+        expect(await Staking.rewardsOf(addr1.address)).to.equal(0);
+      });
+      it("Has no totalSupply", async function () {
+        expect(await Staking.totalSupply()).to.equal(0);
+      });
+    })
+    describe("Carr Management", async function () {
+      it("Has an balance of CARR tokens to distribute", async function () {
+        expect(await CarrToken.balanceOf(Staking.address)).to.equal("5000000000000000000000000");
+      });
+      it("Can receive CARR directly", async function () {
+        await CarrToken.transfer(Staking.address, "1000")
+        expect(await CarrToken.balanceOf(Staking.address)).to.equal("5000000000000000000001000");
+      });
+      it("Can recover CARR to owner address", async function () {
+        await expect(Staking.recoverERC20(CarrToken.address, "1000")).to.emit(Staking, "Recovered").withArgs(CarrToken.address, "1000")
+        expect(await CarrToken.balanceOf(Staking.address)).to.equal("5000000000000000000000000");
+      });
+    });
+  });
+  describe("Active", async function () {
+    it("Accepts deposits", async function () {
+      await CarrToken.approve(Staking.address, qty);  // owner stakes qty
+      await expect(Staking.stake(qty)).to.emit(Staking, 'Staked').withArgs(owner.address, qty);
+      depositTime = (await ethers.provider.getBlock(await ethers.provider.getBlockNumber())).timestamp;
+      expect(await Staking.balanceOf(owner.address)).to.equal(qty);
+    });
+    it("Has no rewards initially", async function () {
+      expect(await Staking.rewardsOf(owner.address)).to.equal("0");
+    });
+    it("Allows deposits from regular users", async function () {
+      await CarrToken.connect(addr1).approve(Staking.address, qty);  // owner stakes qty
+      await expect(Staking.connect(addr1).stake(qty)).to.emit(Staking, 'Staked').withArgs(addr1.address, qty);
+      expect(await Staking.balanceOf(addr1.address)).to.equal(qty);
+    });
+    it("Set the finishTime to 1 year after deposit", async function () {
+      await expect(Staking.setFinish(depositTime + 31536000))
+        .to.emit(Staking, "StakingEnds").withArgs(depositTime + 31536000); // finish 1 year after deposit
+    });
+    it("Verifies interest after 1 month", async function () {
+      await ff(month_in_seconds);
+      expect(await Staking.rewardsOf(owner.address)).to.equal(r.m1);
+    });
+    it("Verifies interest after 2 months", async function () {
+      await ff(2 * month_in_seconds);
+      expect(await Staking.rewardsOf(owner.address)).to.equal(r.m2);
+    });
+    it("Verifies interest after 1 year (- 1 second)", async function () {
+      await ff(12 * month_in_seconds - 1);
+      expect(await Staking.rewardsOf(owner.address)).to.equal(r.m12m1);
+    });
+    it("Verifies interest after 1 year", async function () {
+      await ff(12 * month_in_seconds);
+      expect(await Staking.rewardsOf(owner.address)).to.equal(r.m12);
+    });
+  });
+  describe("Finished", async function () {
+    it("Stops accepting deposits", async function () {
+      // await CarrToken.approve(Staking.address, qty);  // owner stakes qty
+      await expect(Staking.stake(qty)).to.be.revertedWith("Staking period has ended");
+    });
+    it("Stops increasing rewards", async function () {
+      await ff(month_in_seconds * 12 + 1);
+      expect(await Staking.rewardsOf(owner.address)).to.equal(r.m12);
+      await ff(month_in_seconds * 13);
+      expect(await Staking.rewardsOf(owner.address)).to.equal(r.m12);
+    });
+    it("Has expected totalSupply", async function () {
+      expect(await Staking.totalSupply()).to.equal("200000000000000000000000");
+    });
+    it("Allows withdrawals", async function () {
+      // emit Withdrawn(to, amount);
+      await expect(Staking.withdraw(qty))
+        .to.emit(Staking, "Withdrawn").withArgs(owner.address, qty)
+        .to.emit(Staking, 'Staked').withArgs(owner.address, r.m12);
+      expect(await Staking.balanceOf(owner.address)).to.equal(r.m12);
+      expect(await CarrToken.balanceOf(owner.address)).to.equal("4900000000000000000000000");
+    });
+    it("Has expected totalSupply", async function () {
+      expect(await Staking.totalSupply()).to.equal("122140275739388350171449");
+    });
+    it("Allows full withdrawals", async function () {
+      await expect(Staking.withdrawAll())
+        .to.emit(Staking, "Withdrawn").withArgs(owner.address, r.m12)
+        .to.not.emit(Staking, "Staked");
+      expect(await Staking.balanceOf(owner.address)).to.equal(0);
+      expect(await CarrToken.balanceOf(owner.address)).to.equal("4922140275739388350171449");
+    });
+    it("Has expected totalSupply", async function () {
+      expect(await Staking.totalSupply()).to.equal("100000000000000000000000");
+    });
+    it("Allows withdrawals from regular user", async function () {
+      await expect(Staking.connect(addr1).withdraw(qty))
+        .to.emit(Staking, 'Withdrawn').withArgs(addr1.address, qty)
+        .to.emit(Staking, 'Staked').withArgs(addr1.address, "22140274190171270240817");
+      expect(await Staking.balanceOf(addr1.address)).to.equal("22140274190171270240817");
+      expect(await CarrToken.balanceOf(addr1.address)).to.equal("100000000000000000000000");
+      expect(await Staking.totalSupply()).to.equal("22140274190171270240817");
+    });
+    it("Allows regular users to withdrawAll", async function() {
+      await expect(Staking.connect(addr1).withdrawAll())
+        .to.emit(Staking, 'Withdrawn')
+        .to.not.emit(Staking, "Staked");
+      expect(await Staking.balanceOf(addr1.address)).to.equal("0");
+      expect(await CarrToken.balanceOf(addr1.address)).to.equal("122140274190171270240817");
+      expect(await Staking.totalSupply()).to.equal("0");
+    });
+    it("Can receive CARR directly", async function () {
+      await CarrToken.transfer(Staking.address, qty)
+      expect(await CarrToken.balanceOf(Staking.address)).to.equal("5055719450070440379587734");
+    });
+    it("Can recover CARR to owner address", async function () {
+      const q = "55719450070440379587734";
+      await expect(Staking.recoverERC20(CarrToken.address, q)).to.emit(Staking, "Recovered").withArgs(CarrToken.address, q)
+      expect(await CarrToken.balanceOf(Staking.address)).to.equal("5000000000000000000000000");
+    });
+
+  });
+  async function ff(t) {
+    let block = await ethers.provider.getBlock(await ethers.provider.getBlockNumber());
+    const deltaT = depositTime + t - block.timestamp;
+    if (deltaT) {
+      await ethers.provider.send('evm_increaseTime', [deltaT]); // one year
+      await hre.ethers.provider.send('evm_mine');
+    }
+  }
+});
